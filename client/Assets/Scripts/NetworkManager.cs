fa931f28cf41ebfde99cb7d89a77baa7f68b9d23
Chutiphon.k <chutiphon.k@ku.th>
2017-05-08 23:36:39 +0700

    :heavy_plus_sign: Add serializable function to NetworkManager

--- client/Assets/Scripts/NetworkManager.cs
+++ client/Assets/Scripts/NetworkManager.cs
@@ -56,8 +56,95 @@ public class NetworkManager : MonoBehaviour {
 
 	[Serializable]
 	public class PointJSON {
+		public float[] position;
+		public float[] rotation;
 		public PointJSON(SpawnPoint spawnPoint){
-			
+			position = new float[] {
+				spawnPoint.transform.position.x,
+				spawnPoint.transform.position.y,
+				spawnPoint.transform.position.z
+			};
+			rotation = new float[] {
+				spawnPoint.transform.eulerAngles.x,
+				spawnPoint.transform.eulerAngles.y,
+				spawnPoint.transform.eulerAngles.z
+			};	
 		}
 	}
+
+	[Serializable]
+	public class PositionJSON {
+		public float[] position;
+
+		public PositionJSON(Vector3 _position){
+			position = new float[] { _position.x, _position.y, _position.z };
+		}
+	}
+
+	[Serializable]
+	public class RotationJSON {
+		public float[] rotation;
+
+		public RotationJSON(Quaternion _rotation){
+			rotation = new float[] {
+				_rotation.eulerAngles.x,
+				_rotation.eulerAngles.y,
+				_rotation.eulerAngles.z
+			};
+		}
+	}
+
+	[Serializable]
+	public class UserJSON {
+		public string name;
+		public float[] position;
+		public float[] rotation;
+		public int health;
+
+		public static UserJSON CreateFromJSON(string data){
+			return JsonUtility.FromJson<UserJSON>(data);
+		}
+	}
+
+	[Serializable]
+	public class HealthChangeJSON {
+		public string name;
+		public int healthChange;
+		public string from ;
+		public bool isEnemy;
+
+		public HealthChangeJSON(string _name, int _healthChange, string _from, bool _isEnemy){
+			name = _name;
+			healthChange = _healthChange;
+			from = _from;
+			isEnemy = _isEnemy;
+		}
+	}
+
+	[Serializable]
+	public class EnemiesJSON {
+		public List<UserJSON> enemies;
+
+		public static EnemiesJSON CreateFromJSON(string data){
+			return JsonUtility.FromJson<EnemiesJSON>(data);
+		}
+	}	
+
+	[Serializable]
+	public class ShootJSON {
+		public string name;
+		public static ShootJSON CreateFromJSON(string data) {
+			return JsonUtility.FromJson<ShootJSON>(data);
+		}
+	}	
+
+	[Serializable]
+	public class UserHealthJSON {
+		public string name;
+		public int health;
+
+		public static UserHealthJSON CreateFromJSON(string data){
+			return JsonUtility.FromJson<UserHealthJSON>(data);			
+		}
+	}	
 }
