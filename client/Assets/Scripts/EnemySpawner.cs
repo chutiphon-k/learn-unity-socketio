e0295fa0c3bb749af19db47031713d4b862949f2
Chutiphon.k <chutiphon.k@ku.th>
2017-05-24 16:26:52 +0700

    :bug: Fix bug when enemy health 0, it not destroy

--- client/Assets/Scripts/EnemySpawner.cs
+++ client/Assets/Scripts/EnemySpawner.cs
@@ -23,6 +23,7 @@ public class EnemySpawner : MonoBehaviour {
 	
 	public void SpawnEnemies(NetworkManager.EnemiesJSON enemiesJSON){
 		foreach(NetworkManager.UserJSON enemyJSON in enemiesJSON.enemies){
+			if(enemyJSON.health <= 0) continue;
 			Vector3 position = new Vector3(enemyJSON.position[0], enemyJSON.position[1], enemyJSON.position[2]);
 			Quaternion rotation = Quaternion.Euler(enemyJSON.rotation[0], enemyJSON.rotation[1], enemyJSON.rotation[2]);
 			GameObject newEnemy = Instantiate(enemy, position, rotation) as GameObject;
